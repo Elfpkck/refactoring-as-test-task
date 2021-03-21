b63b7514866a2001a950af8c834b697e4b5cc04a
Andrii Liekariev <elfpkck@gmail.com>
2021-03-21 20:02:32 +0200

    hide imports from analyzer inside the task

--- tasks.py
+++ tasks.py
@@ -1,7 +1,6 @@
 import os
 from typing import Dict
 
-from analyzer import Analyzer, TrainingConfig
 from celery_app import app
 
 TASK_NAME = os.getenv('TASK_NAME')
@@ -9,4 +8,6 @@ TASK_NAME = os.getenv('TASK_NAME')
 
 @app.task(name=TASK_NAME)
 def analyze(data: Dict) -> Dict:
+    from analyzer import Analyzer, TrainingConfig
+
     return Analyzer(data.get('payload', {}), TrainingConfig()).analyze()
